d36a6f75215f001dee4638bc0796ce8208f664ae
Delfland <d_dallison@hotmail.com>
2023-02-26 21:47:00 +0000

    organised backend and frontend into folders and created spring server

---
+++ back_end/src/test/java/nom_nom/NomNomApplicationTests.java
@@ -0,0 +1,13 @@
+package nom_nom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomNomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
