7cb8aea8d4f3469892b1efc84efe5c5ca6d1e17d
jilleatkins <jilleatkins@gmail.com>
2023-03-02 15:20:22 +0000

    Added diseased content

--- front_end/src/components/Welcome.js
+++ front_end/src/components/Welcome.js
@@ -2,71 +2,79 @@ import { Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
-    <div className="relative flex h-72 min-h-screen flex-col justify-center bg-gradient-to-b from-orange-400 via-[#f4a134] to-yellow-300 p-10 text-center">
-      <div className="flex h-40 justify-center">
-        <h1 className="body-font animate__bounceInDown font-nomnom text-[140px] text-white animate__animated">
-          nomnom.
-        </h1>
-      </div>
-      <p className="animate__pulse animate__delay-2s body-font flex justify-center p-1 font-body text-3xl text-white animate__animated">
-        Cooking made fun!
-      </p>
-      <br />
-      <div className="flex justify-center pt-10">
-        <p className="text-1xl mr-1 mt-0.5 font-nomnombold text-white">nomnom.</p>
-        <p className="body-font text-1xl font-body text-white">
-          is an educational app that turns cooking into a fun group activity between grown-up and
-          and child.
+    <div className="align-center relative flex h-72 min-h-screen flex-col justify-center bg-[#f35b04] text-center">
+      <div className="mx-auto w-4/6 rounded-3xl bg-white px-12 pt-10">
+        <div className="flex h-40 justify-center">
+          <h1 className="animate__bounceInDown font-nomnombold text-[140px] tracking-tighter text-[#3d348b] animate__animated">
+            nomnom.
+          </h1>
+        </div>
+        <p className="animate__pulse animate__delay-2s body-font font-nunito flex justify-center text-[32px] font-bold text-[#f18701] animate__animated">
+          Cooking made fun!
         </p>
+        <br />
+        <div className="flex justify-center pt-5">
+          <p className="mr-1 mt-0.5 font-nomnombold text-[22px] text-[#f7b801]">nomnom.</p>
+          <p className="body-font font-nunito text-[22px] text-[#f7b801]">
+            is an educational app that turns cooking into a fun group activity between grown-up and
+            and child.
+          </p>
+        </div>
+        <br />
+        <div className="z-0 mx-auto mb-20 inline-flex h-60 w-full items-center justify-center gap-20">
+          <Link
+            to="/login"
+            className="body-font ... ease inline-flex h-56 w-56 flex-col items-center justify-center rounded-full bg-yellow-300 p-20 text-center font-nomnom text-2xl text-[#f35b04] shadow-xl transition delay-100 duration-200 ease-in-out hover:-translate-y-2 hover:scale-105 hover:bg-yellow-200">
+            Login
+            <br />
+            <div className="mt-auto p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="inline-flex h-6 w-6">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+                />
+              </svg>
+            </div>
+          </Link>
+          <Link
+            to="/allrecipes"
+            className="body-font ... ease inline-flex h-56 w-56 flex-col items-center justify-center rounded-full bg-yellow-300 p-20 text-center font-nomnom text-2xl text-[#f35b04] shadow-xl transition delay-100 duration-200 ease-in-out hover:-translate-y-2 hover:scale-105 hover:bg-yellow-200">
+            Search
+            <br />
+            Recipes
+            <br />
+            <div className="mt-auto p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-6 w-6">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                />
+              </svg>
+            </div>
+          </Link>
+        </div>
+        <div>
+          <img
+            className="animate__bounceInLeft animate__delay-1s absolute left-12 -bottom-1 z-40 flex h-80 max-w-full animate__animated"
+            src="https://nomnomcc.s3.amazonaws.com/nomnomwelcome.png"
+            alt="chefs"
+          />
+        </div>
       </div>
-      <br />
-      <div className="z-0 mx-auto mb-20 inline-flex h-60 w-full items-center justify-center gap-20">
-        <Link
-          to="/login"
-          className="body-font ... ease inline-flex h-56 w-56 flex-col items-center justify-center rounded-full bg-yellow-300 p-20 text-center font-nomnom text-2xl text-orange-500 shadow-xl transition delay-100 duration-200 ease-in-out hover:-translate-y-2 hover:scale-105 hover:bg-yellow-200">
-          Login
-          <br />
-          <div className="mt-auto p-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="inline-flex h-6 w-6">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-              />
-            </svg>
-          </div>
-        </Link>
-        <Link
-          to="/allrecipes"
-          className="body-font ... ease inline-flex h-56 w-56 flex-col items-center justify-center rounded-full bg-yellow-300 p-20 text-center font-nomnom text-2xl text-orange-500 shadow-xl transition delay-100 duration-200 ease-in-out hover:-translate-y-2 hover:scale-105 hover:bg-yellow-200">
-          Search
-          <br />
-          Recipes
-          <br />
-          <div className="mt-auto p-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-6 w-6">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              />
-            </svg>
-          </div>
-        </Link>
-      </div>
-     w
     </div>
   );
 };
--- front_end/src/container/NomNomContainer.js
+++ front_end/src/container/NomNomContainer.js
@@ -1,45 +1,36 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import Login from '../components/user/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from '../components/Welcome';
 import AllRecipes from '../components/recipes/AllRecipes';
-import CreateRecipe from '../components/CreateRecipe';
-import UserProfile from '../components/UserProfile';
-import { useState } from 'react';
+import CreateRecipe from '../components/recipes/CreateRecipe';
+import UserProfile from '../components/user/UserProfile';
+import { useState, useEffect } from 'react';
 import Request from '../helpers/Request';
 
 const NomNomContainer = () => {
+  const [allRecipes, setAllRecipes] = useState([]);
+  const [profile, setProfile] = useState([]);
+  const [customRecipes, setCustomRecipes] = useState([]);
+  const [allCustomRecipes, setAllCustomRecipes] = useState([]);
+  const [allFaveRecipes, setAllFaveRecipes] = useState([]);
 
-  const [allRecipes, setAllRecipes] = useState([])
-  const [profile, setProfile] = useState([])
-  const [customRecipes, setCustomRecipes] = useState([])
-  const [allCustomRecipes, setAllCustomRecipes] = useState([])
-  const [allFaveRecipes, setAllFaveRecipes] = useState([])
+  // useEffect(() => {
+  //   const request = new Request();
 
-  useEffect(() => {
-    const request = new Request();
+  //   const recipePromise = request.get('/api/recipes');
 
-    const recipePromise = request.get('/api/recipes');
-
-    Promise([allRecipePromise])
-    .then((data) => {
-      setAllRecipes(data[0])
-    })
-  },[])
+  //   Promise([allRecipePromise]).then((data) => {
+  //     setAllRecipes(data[0]);
+  //   });
+  // }, []);
 
   // const findAllRecipesById = (id) => {
   //   return allRecipes.find((recipe) => {
   //     return recipe.id === parseInt(id);
   // })
   // }
-
-  }
-  
-
-
-
-
-
-
 
   return (
     <>
