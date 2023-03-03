4fa23ec4d31f7c3fa54c4b85d23164bbe7330160
Brian Dickson <brianlawrencedickson@gmail.com>
2023-03-03 19:01:13 +0000

    added all recipee elements to detail

--- front_end/src/components/recipes/RecipeDetail.js
+++ front_end/src/components/recipes/RecipeDetail.js
@@ -3,7 +3,24 @@ import { useLocation } from 'react-router-dom';
 const RecipeDetail = () => {
   const recipe = useLocation().state;
 
-  return <p>{recipe.name}</p>;
+  return (
+    <ul>
+      <li>{recipe.name}</li>
+      <li>{recipe.image_url}</li>
+      <p>Prep Time:</p>
+      <li>{recipe.prepTime}</li>
+      <p>minutes</p>
+      <p>Cook Time:</p>
+      <li>{recipe.cookTime}</li>
+      <p>minutes</p>
+      <p>Servings</p>
+      <li>{recipe.servings}</li>
+      <p>Meal Type:</p>
+      <li>{recipe.mealType}</li>
+      <p>Extra Equipment:</p>
+      <li>{recipe.extraEquip}</li>
+    </ul>
+  );
 };
 
 export default RecipeDetail;
