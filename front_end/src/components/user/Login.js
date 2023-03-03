cc7c4603b99790dacf9cdca28a38e2bd556f7f13
jilleatkins <jilleatkins@gmail.com>
2023-03-03 10:58:07 +0000

    Tidied up formatting

--- front_end/src/components/user/Login.js
+++ front_end/src/components/user/Login.js
@@ -2,7 +2,7 @@
 const Login = () => {
   return (
     <div className="flex h-72 min-h-screen flex-col justify-center bg-food_background2 p-10 text-center">
-      <div className="mx-auto h-5/6 w-4/6 rounded-3xl bg-[#fffffff3] px-12">
+      <div className="mx-auto flex h-5/6 w-4/6 justify-center rounded-3xl bg-[#fffffff3] px-12">
         <div className="h-40 flex-col justify-center">
           <h1 className="body-font animate__bounceInDown my-16 flex justify-center font-nomnombold text-9xl tracking-tight text-[#3E8B46] animate__animated">
             login
@@ -13,7 +13,7 @@ const Login = () => {
               type="email"
               placeholder="Email:"
               className="placeholder-center body-font mx-auto inline-flex w-full max-w-md rounded-md border-2 border-gray-400 
-         py-2 px-4 font-nunito focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#95C938]"
+         bg-[#cdf18b] py-2 px-4 font-nunito focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#95C938]"
             />
             <br />
             <input
